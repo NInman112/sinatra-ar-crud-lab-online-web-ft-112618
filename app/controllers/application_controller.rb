7b130081d99cfe6a892923b10e052305db1befae
Nick Inman <nick@theoatmeal.com>
2019-01-26 19:26:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,6 @@ class ApplicationController < Sinatra::Base
 
   delete '/articles/:id/' do
     @article = Article.find(params[:id])
-    @article.destory(params[:article])
+    @article.destroy(params[:id])
   end
 end
